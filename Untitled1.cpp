db7dc58ab3db84d72999e5071fa30c0e6be2848b
Metin Köroğlu <metinnkoroglu@gmail.com>
2022-12-21 23:36:01 +0300

    Harf notu hesaplama özelliği eklendi.

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -11,12 +11,36 @@ int main()
    ort=(vize*0.3)+(final*0.7);
    cout<<"Ortalama : "<<ort<<endl;
    
-   if(ort<50)
+   if(101>ort && ort>89)
    {
-   	cout<<"Kaldiniz.";
+   	cout<<"AA";
+   }
+   else if(90>ort && ort>84)
+   {
+   	cout<<"BA";
+   }
+     else if(85>ort && ort>79)
+   {
+   	cout<<"BB";
+   }
+     else if(80>ort && ort>74)
+   {
+   	cout<<"CB";
+   }
+     else if(75>ort && ort>69)
+   {
+   	cout<<"CC";
+   }
+     else if(70>ort && ort>64)
+   {
+   	cout<<"DC";
+   }
+     else if(65>ort && ort>54)
+   {
+   	cout<<"DD";
    }
    else
    {
-   	cout<<"Gectiniz.";
+   	cout<<"FF";
    }
 }
